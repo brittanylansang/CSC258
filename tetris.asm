504bd276130c17d88b2d16850fecd115a660289a
Gregory G <gr.gismondi@gmail.com>
2024-03-27 15:25:44 -0400

    Redid Grid Code
    Refactored how the grid was drawn to increase the speed of it

--- tetris.asm
+++ tetris.asm
@@ -82,66 +82,19 @@ main:
     addi $a3, $zero, 1      # set height of line to 1
     jal draw_rectangle      # call the rectangle-drawing function
     
-    # draw grid
-    addi $a0, $zero, 1     # set x coordinate of line to 1
-    addi $a1, $zero, 0     # set y coordinate of line to 0
-    addi $a2, $zero, 1     # set length of line to 10
-    addi $a3, $zero, 31    # set height of line to 31
-    jal even_grid     # call the grid-drawing function
+    # draw dark part of grid
+    addi $a0, $zero, 1      # set x coordinate of line to 0
+    addi $a1, $zero, 0      # set y coordinate of line to 31
+    addi $a2, $zero, 10     # set length of line to 31
+    addi $a3, $zero, 31     # set height of line to 1
+    jal draw_dark_grid      # call the rectangle-drawing function
     
-    addi $a0, $zero, 3     # set x coordinate of line to 1
-    addi $a1, $zero, 0     # set y coordinate of line to 0
-    addi $a2, $zero, 1     # set length of line to 10
-    addi $a3, $zero, 31    # set height of line to 31
-    jal even_grid     # call the grid-drawing function
-    
-    addi $a0, $zero, 5     # set x coordinate of line to 1
-    addi $a1, $zero, 0     # set y coordinate of line to 0
-    addi $a2, $zero, 1     # set length of line to 10
-    addi $a3, $zero, 31    # set height of line to 31
-    jal even_grid     # call the grid-drawing function
-    
-    addi $a0, $zero, 7     # set x coordinate of line to 1
-    addi $a1, $zero, 0     # set y coordinate of line to 0
-    addi $a2, $zero, 1     # set length of line to 10
-    addi $a3, $zero, 31    # set height of line to 31
-    jal even_grid     # call the grid-drawing function
-    
-    addi $a0, $zero, 9     # set x coordinate of line to 1
-    addi $a1, $zero, 0     # set y coordinate of line to 0
-    addi $a2, $zero, 1     # set length of line to 10
-    addi $a3, $zero, 31    # set height of line to 31
-    jal even_grid     # call the grid-drawing function
-    
-    addi $a0, $zero, 2     # set x coordinate of line to 1
-    addi $a1, $zero, 0     # set y coordinate of line to 0
-    addi $a2, $zero, 1     # set length of line to 10
-    addi $a3, $zero, 31    # set height of line to 31
-    jal odd_grid     # call the grid-drawing function
-    
-    addi $a0, $zero, 4     # set x coordinate of line to 1
-    addi $a1, $zero, 0     # set y coordinate of line to 0
-    addi $a2, $zero, 1     # set length of line to 10
-    addi $a3, $zero, 31    # set height of line to 31
-    jal odd_grid     # call the grid-drawing 
-    
-    addi $a0, $zero, 6     # set x coordinate of line to 1
-    addi $a1, $zero, 0     # set y coordinate of line to 0
-    addi $a2, $zero, 1     # set length of line to 10
-    addi $a3, $zero, 31    # set height of line to 31
-    jal odd_grid     # call the grid-drawing function
-
-    addi $a0, $zero, 8     # set x coordinate of line to 1
-    addi $a1, $zero, 0     # set y coordinate of line to 0
-    addi $a2, $zero, 1     # set length of line to 10
-    addi $a3, $zero, 31    # set height of line to 31
-    jal odd_grid     # call the grid-drawing function
-    
-    addi $a0, $zero, 10     # set x coordinate of line to 1
-    addi $a1, $zero, 0     # set y coordinate of line to 0
-    addi $a2, $zero, 1     # set length of line to 10
-    addi $a3, $zero, 31    # set height of line to 31
-    jal odd_grid     # call the grid-drawing function
+    # draw light part of grid
+    addi $a0, $zero, 1      # set x coordinate of line to 0
+    addi $a1, $zero, 0      # set y coordinate of line to 31
+    addi $a2, $zero, 10     # set length of line to 31
+    addi $a3, $zero, 31     # set height of line to 1
+    jal draw_light_grid     # call the rectangle-drawing function
 
     #draw I piece
     la $t7, current_i_x  # Load the piece_I_x label's address into $t7
@@ -167,13 +120,84 @@ main:
 # - $t4: the colour value to draw on the bitmap
 # - $t5: the bitmap location for the end of the horizontal line.
 # - $t6: the bitmap location for the end of the vertical line.
-# - $t7: stores the colour gray for the grid
-# - $t8: stores the colour light gray for the grid
+# - $t7: stores the colour for the function
+# - $t8: Pixel shift amount
+
+draw_light_grid:
+add $t8, $zero, $zero       # Sets initial offset to 0
+sll $t2, $a1, 7             # convert vertical offset to pixels (by multiplying $a1 by 128, equivalent to logical left shifts)
+sll $t6, $a3, 7             # convert height of rectangle from pixels to rows of bytes (by multiplying $a3 by 128)
+add $t6, $t2, $t6           # calculate value of $t2 for the last line in the rectangle.
+la $t7, light_checkerboard  # fetch wall_colour label address
+
+outer_top_lg:
+sll $t1, $a0, 2         # convert horizontal offset to pixels (by multiplying $a0 by 4)
+sll $t5, $a2, 2         # convert length of line from pixels to bytes (by multiplying $a2 by 4)
+add $t5, $t1, $t5       # calculate value of $t1 for end of the horizontal line.
+
+beq $t8, $zero, no_offset # Checks if the offset is equal to zero - if so breaks to no_offset label
+
+# Offset is equal to 4
+add $t1, $t8, $t1       # Adds offset to starting pixel
+add $t5, $t8, $t5       # Adds offset to ending pixel
+add $t8, $zero, $zero   # Resets offset to 0 - makes sure next row won't offset
+j inner_top_lg          # Jumps to beginning of loop
+
+no_offset:
+addi $t8, $zero, 4      # Sets offset to 4 - makes sure next row will offset
+
+inner_top_lg:
+add $t3, $t1, $t2           # store the total offset of the starting pixel (relative to $t0)
+add $t3, $t0, $t3           # calculate the location of the starting pixel ($t0 + offset)
+
+lw $t4, 0($t7)              # load into $t4 = darker gray for the walls
+
+sw $t4, 0($t3)              # paint the current unit on the first row yellow
+addi $t1, $t1, 8            # move horizontal offset to the right by two pixels
+beq $t1, $t5, inner_end_lg  # break out of the line-drawing loop
+j inner_top_lg              # jump to the start of the inner loop
+inner_end_lg:
+
+addi $t2, $t2, 128          # move vertical offset down by one line
+beq $t2, $t6, outer_end_lg  # on last line, break out of the outer loop
+j outer_top_lg              # jump to the top of the outer loop
+outer_end_lg:
+jr $ra                      # return to calling program
+
+draw_dark_grid:
+sll $t2, $a1, 7             # convert vertical offset to pixels (by multiplying $a1 by 128, equivalent to logical left shifts)
+sll $t6, $a3, 7             # convert height of rectangle from pixels to rows of bytes (by multiplying $a3 by 128)
+add $t6, $t2, $t6           # calculate value of $t2 for the last line in the rectangle.
+la $t7, dark_checkerboard   # fetch wall_colour label address
+
+outer_top_dg:
+sll $t1, $a0, 2         # convert horizontal offset to pixels (by multiplying $a0 by 4)
+sll $t5, $a2, 2         # convert length of line from pixels to bytes (by multiplying $a2 by 4)
+add $t5, $t1, $t5       # calculate value of $t1 for end of the horizontal line.
+
+inner_top_dg:
+add $t3, $t1, $t2           # store the total offset of the starting pixel (relative to $t0)
+add $t3, $t0, $t3           # calculate the location of the starting pixel ($t0 + offset)
+
+lw $t4, 0($t7)              # load into $t4 = darker gray for the walls
+
+sw $t4, 0($t3)              # paint the current unit on the first row yellow
+addi $t1, $t1, 4            # move horizontal offset to the right by one pixel
+beq $t1, $t5, inner_end_dg  # break out of the line-drawing loop
+j inner_top_dg              # jump to the start of the inner loop
+inner_end_dg:
+
+addi $t2, $t2, 128          # move vertical offset down by one line
+beq $t2, $t6, outer_end_dg  # on last line, break out of the outer loop
+j outer_top_dg              # jump to the top of the outer loop
+outer_end_dg:
+jr $ra                      # return to calling program
 
 draw_rectangle:
 sll $t2, $a1, 7         # convert vertical offset to pixels (by multiplying $a1 by 128, equivalent to logical left shifts)
 sll $t6, $a3, 7         # convert height of rectangle from pixels to rows of bytes (by multiplying $a3 by 128)
 add $t6, $t2, $t6       # calculate value of $t2 for the last line in the rectangle.
+
 outer_top:
 sll $t1, $a0, 2         # convert horizontal offset to pixels (by multiplying $a0 by 4)
 sll $t5, $a2, 2         # convert length of line from pixels to bytes (by multiplying $a2 by 4)
@@ -197,111 +221,6 @@ beq $t2, $t6, outer_end     # on last line, break out of the outer loop
 j outer_top                 # jump to the top of the outer loop
 outer_end:
 jr $ra                      # return to calling program
-
-even_grid:
-li $t5, 0 # store 0 in $t5, this will be used to check to switch colours
-
-sll $t2, $a1, 7         # convert vertical offset to pixels (by multiplying $a1 by 128, equivalent to logical left shifts)
-sll $t6, $a3, 7         # convert height of rectangle from pixels to rows of bytes (by multiplying $a3 by 128)
-add $t6, $t2, $t6       # calculate value of $t2 for the last line in the rectangle.
-
-outer_even_grid_top:
-sll $t1, $a0, 2         # convert horizontal offset to pixels (by multiplying $a0 by 4)
-# sll $t5, $a2, 2         # convert length of line from pixels to bytes (by multiplying $a2 by 4)
-# add $t5, $t1, $t5       # calculate value of $t1 for end of the horizontal line.
-
-inner_even_grid_top:
-la $t7, dark_checkerboard     # fetch wall_colour label address
-lw $t8, 0($t7)          # load into $t8 = dark grid colour
-la $t7, light_checkerboard     # fetch wall_colour label address
-lw $t9, 0($t7)          # load into $t9 = light grid colour
-
-# bne $t4, $t8, switch_colour_gray # if the grid colour is light gray, we need to switch it to dark gray 
-# bne $t4, $t9, switch_colour_light_gray # if the grid colour is dark gray, we need to switch it to light gray 
-check_even_colour:
-bne $t5, 0, light_gray_even # if t5 is not zero draw light
-
-#draw dark first iteration
-dark_gray_even:
-add $t4, $t8, $zero # load dark gray colour
-j draw_even # jump to where we draw the pixels
-
-light_gray_even:
-add $t4, $t9, $zero # load light gray colour
-j draw_even # jump to where we draw the 
-
-draw_even:
-add $t3, $t1, $t2           # store the total offset of the starting pixel (relative to $t0)
-add $t3, $t0, $t3           # calculate the locationq of the starting pixel ($t0 + offset)
-sw $t4, 0($t3)              # paint the current unit on the first row the colour in $t4
-addi $t2, $t2, 128          # move vertical offset down by one line
-beq $t2, $t6, inner_even_grid_end     # on last line, break out of the outer loop
-# j outer_grid_top                 # jump to the top of the outer loop
-# outer_grid_end:
-# jr $ra
-
-
-next_even:
-addi $t5, $t5, 1       # Increment the alternate counter
-andi $t5, $t5, 1       # Keep $t3 value as 0 or 1
-
-j outer_even_grid_top                 # jump to the top of the outer loop
-inner_even_grid_end:
-jr $ra
-
-odd_grid:
-# store initial grid colour in $t4
-# la $t7, grid_dark_colour     # fetch grid_dark_colour label address
-# lw $t4, 0($t7)          # load into $t4 = darker gray for the walls
-li $t5, 0
-
-sll $t2, $a1, 7         # convert vertical offset to pixels (by multiplying $a1 by 128, equivalent to logical left shifts)
-sll $t6, $a3, 7         # convert height of rectangle from pixels to rows of bytes (by multiplying $a3 by 128)
-add $t6, $t2, $t6       # calculate value of $t2 for the last line in the rectangle.
-outer_odd_grid_top:
-sll $t1, $a0, 2         # convert horizontal offset to pixels (by multiplying $a0 by 4)
-# sll $t5, $a2, 2         # convert length of line from pixels to bytes (by multiplying $a2 by 4)
-# add $t5, $t1, $t5       # calculate value of $t1 for end of the horizontal line.
-
-inner_odd_grid_top:
-
-la $t7, dark_checkerboard     # fetch wall_colour label address
-lw $t8, 0($t7)          # load into $t8 = dark grid colour
-
-la $t7, light_checkerboard     # fetch wall_colour label address
-lw $t9, 0($t7)          # load into $t9 = light grid colour
-
-# bne $t4, $t8, switch_colour_gray # if the grid colour is light gray, we need to switch it to dark gray 
-# bne $t4, $t9, switch_colour_light_gray # if the grid colour is dark gray, we need to switch it to light gray 
-check_odd_colour:
-bne $t5, 0, dark_gray_odd # if t7 is not zero draw draw light
-
-light_gray_odd:
-add $t4, $t9, $zero # load light gray colour
-j paint_odd # jump to where we draw the pixels
-
-dark_gray_odd:
-add $t4, $t8, $zero # load dark gray colour
-j paint_odd # jump to where we draw the pixels
-
-paint_odd:
-add $t3, $t1, $t2           # store the total offset of the starting pixel (relative to $t0)
-add $t3, $t0, $t3           # calculate the locationq of the starting pixel ($t0 + offset)
-sw $t4, 0($t3)              # paint the current unit on the first row the colour in $t4
-addi $t2, $t2, 128          # move vertical offset down by one line
-beq $t2, $t6, inner_odd_grid_end     # on last line, break out of the outer loop
-# j outer_grid_top                 # jump to the top of the outer loop
-# outer_grid_end:
-# jr $ra
-
-
-next_odd:
-addi $t5, $t5, 1       # Increment the alternate counter
-andi $t5, $t5, 1       # Keep $t3 value as 0 or 1
-
-j outer_odd_grid_top                 # jump to the top of the outer loop
-inner_odd_grid_end:
-jr $ra
 
 draw_tetromino:
 	sll $t2, $a1, 7         # convert vertical offset to pixels (by multiplying $a1 by 128, equivalent to logical left shifts)
